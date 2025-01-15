ab7d28764f6fa12507b04c254c7044a8a3e259f3
Bryan Hurley <bch92326@email.vccs.edu>
2025-01-15 09:04:57 -0500

    Fixed Branch

--- Checkpoint2.14.cpp
+++ Checkpoint2.14.cpp
@@ -6,28 +6,21 @@ variables then display them on the screen.
 */
 
 #include <iostream>
-
+#include <string>
 int main()
 {
 	char firstInitial;
 	char middleInitial;
 	char lastInitial;
+	std::string firstName;
 	std::cout << "Please enter the initial of your first name:";
 	std::cin >> firstInitial;
 	std::cout << "Please enter the initial of your middle name:";
 	std::cin >> middleInitial;
 	std::cout << "Please enter the initial of your last name:";
 	std::cin >> lastInitial;
-	std::cout << "Your initals are: " << firstInitial << middleInitial << lastInitial;
+	std::cout << "Your initals are: " << firstInitial << middleInitial << lastInitial << std::endl;
+	std::cout << "Please enter your first name:";
+	std::cin >> firstName;
+	std::cout << "Your first name is : " << firstName;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
